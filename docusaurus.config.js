bdb1cf1e4f0cae4bcd30676b201592f6a1a8089b
Codilion <radiantvow@protonmail.com>
2023-10-23 02:35:01 +0200

    Update GitHub repo link in the header

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,7 +74,7 @@ const config = {
           },
           // {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/0LNetworkCommunity/',
             label: 'GitHub',
             position: 'right',
           },
