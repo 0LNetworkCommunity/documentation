9381fd82957bc025bedb8359ddcdcb866732420a
0o-de-lally <1364012+0o-de-lally@users.noreply.github.com>
2024-07-26 13:49:09 -0400

    test fixing previews with  baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,15 +7,15 @@ const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '0L Network Documentation',
+  title: 'Open Libra Documentation',
   tagline: '...',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://0lnetwork.dev',
+  url: 'https://docs.openlibra.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: process.env.GITHUB_ACTIONS ? `${process.env.BASE_URL}/` : "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
